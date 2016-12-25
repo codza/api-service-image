aa481b8bc799f8a65812bde2696689d7f2185faa
codza <n.lodza@yahoo.fr>
2016-12-25 02:55:29 +0100

    adding the route in the bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
-            new AppBundle\AppBundle(),
             new Extia\ServiceImageBundle\ServiceImageBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
